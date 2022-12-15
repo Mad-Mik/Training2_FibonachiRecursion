e961354d967b16bddcc1ec67af2504735f101dd6
mikhail.madoyan <mikhail.madoyan@gmail.com>
2022-12-15 22:53:00 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Training2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Fibonachi Recursion
+            string number = Console.ReadLine();
+            int y = Convert.ToInt32(number);
+            int x = Fibonachi(y);
+            Console.WriteLine(x);
+        }
+        static int Fibonachi(int n)
+
+        {
+            if (n == 0 || n == 1)
+                return n;
+
+            else
+                return Fibonachi(n - 1) + Fibonachi(n - 2);
+        }
+
+
+
+    }
+}
+
